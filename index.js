024ab0454b0cdec9f6bd5a3014408a20bec1df3e
SergeyUni <sergeyleb0402@outlook.com>
2019-06-23 11:59:58 +1000

    update index js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('index.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const app = express();
+const path = require('path');
+
+app.get('/', function(req, res) {
+  res.json()
+})
